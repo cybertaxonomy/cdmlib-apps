1123cde9381486e3ff4a266fd546f07b5e305ae0
Andreas Müller <a.mueller@bgbm.org>
2011-12-06 13:13:30 +0000

    remove imports

--- app-import/src/main/java/eu/etaxonomy/cdm/app/common/TestActivator.java
+++ app-import/src/main/java/eu/etaxonomy/cdm/app/common/TestActivator.java
@@ -9,10 +9,7 @@
 
 package eu.etaxonomy.cdm.app.common;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.net.URI;
 import java.util.UUID;
 
 import org.apache.log4j.Logger;
@@ -20,26 +17,18 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.transaction.TransactionStatus;
 
 import eu.etaxonomy.cdm.api.application.CdmApplicationController;
-import eu.etaxonomy.cdm.api.application.CdmIoApplicationController;
-import eu.etaxonomy.cdm.api.service.config.ITaxonServiceConfigurator;
-import eu.etaxonomy.cdm.api.service.config.TaxonServiceConfiguratorImpl;
-import eu.etaxonomy.cdm.api.service.pager.Pager;
 import eu.etaxonomy.cdm.common.monitor.DefaultProgressMonitor;
 import eu.etaxonomy.cdm.common.monitor.IProgressMonitor;
 import eu.etaxonomy.cdm.database.DbSchemaValidation;
 import eu.etaxonomy.cdm.database.ICdmDataSource;
+import eu.etaxonomy.cdm.io.common.CdmDefaultImport;
 import eu.etaxonomy.cdm.io.common.IImportConfigurator.CHECK;
-import eu.etaxonomy.cdm.model.common.IdentifiableEntity;
-import eu.etaxonomy.cdm.model.description.Distribution;
-import eu.etaxonomy.cdm.model.description.PresenceTerm;
-import eu.etaxonomy.cdm.model.description.TaxonDescription;
-import eu.etaxonomy.cdm.model.location.NamedArea;
-import eu.etaxonomy.cdm.model.location.TdwgArea;
+import eu.etaxonomy.cdm.io.sdd.in.SDDImportConfigurator;
+import eu.etaxonomy.cdm.model.description.CategoricalData;
+import eu.etaxonomy.cdm.model.description.Modifier;
+import eu.etaxonomy.cdm.model.description.State;
+import eu.etaxonomy.cdm.model.description.StateData;
 import eu.etaxonomy.cdm.model.name.NomenclaturalCode;
-import eu.etaxonomy.cdm.model.taxon.Taxon;
-import eu.etaxonomy.cdm.model.taxon.TaxonBase;
-import eu.etaxonomy.cdm.model.taxon.TaxonNode;
-import eu.etaxonomy.cdm.persistence.query.MatchMode;
 
 /**
  * @author a.babadshanjan
@@ -50,15 +39,16 @@ public class TestActivator {
 	private static final Logger logger = Logger.getLogger(TestActivator.class);
 
 	//static final Source faunaEuropaeaSource = FaunaEuropaeaSources.faunEu();
-	static final ICdmDataSource cdmDestination = CdmDestinations.localH2();
+	static final ICdmDataSource cdmDestination = CdmDestinations.cdm_test_local_mysql();
+//	static final ICdmDataSource cdmDestination = CdmDestinations.localH2();
 //	static final ICdmDataSource cdmDestination = CdmDestinations.cdm_flora_central_africa_production();
 //	static final ICdmDataSource cdmDestination = CdmDestinations.cdm_edit_cichorieae_preview();
 	
 	
 	static final int limitSave = 2000;
 
-//	static final CHECK check = CHECK.CHECK_AND_IMPORT;
-	static final CHECK check = CHECK.CHECK_ONLY;
+	static final CHECK check = CHECK.IMPORT_WITHOUT_CHECK;
+//	static final CHECK check = CHECK.CHECK_ONLY;
 //	static DbSchemaValidation dbSchemaValidation = DbSchemaValidation.NONE;
 //	static DbSchemaValidation dbSchemaValidation = DbSchemaValidation.UPDATE;
 	static DbSchemaValidation dbSchemaValidation = DbSchemaValidation.VALIDATE;
@@ -87,26 +77,34 @@ public class TestActivator {
 //		app = CdmApplicationController.NewInstance(resource, destination, dbSchemaValidation, false, progressMonitor, listeners);
 		app = CdmApplicationController.NewInstance(resource, destination, dbSchemaValidation, false, progressMonitor);
 		
-		TransactionStatus txStatus = app.startTransaction();
-		List<TaxonNode> nodeList = app.getTaxonNodeService().list(null, null, null, null, null);
-		List<TaxonBase> taxonList = new ArrayList<TaxonBase>();
-//		UUID uuidArea = UUID.fromString("c6a2c418-1aee-448f-9836-44d85f9dd139");
-		UUID uuidArea = UUID.fromString("1ca78cd4-7c24-46e9-a45a-ea96a2bb0ecd");
-		NamedArea area = (NamedArea)app.getTermService().find(uuidArea);
-		PresenceTerm status = (PresenceTerm)app.getTermService().find(UUID.fromString("cef81d25-501c-48d8-bbea-542ec50de2c2"));
- 		for (TaxonNode node:nodeList){
-			if (node.getClassification() != null){
-				Taxon taxon = node.getTaxon();
-				TaxonDescription desc = TaxonDescription.NewInstance(taxon, false);
-				desc.setTitleCache("Full area distribution", true);
-				//only for test
-				Distribution distr = Distribution.NewInstance(area, status);
-				desc.addElement(distr);
-				taxonList.add(taxon);
-			}
-		}
-		app.getTaxonService().saveOrUpdate(taxonList);
-		app.commitTransaction(txStatus);
+		TransactionStatus tx = app.startTransaction();
+		State state = (State)app.getTermService().find(UUID.fromString("881b9c80-626d-47a6-b308-a63ee5f4178f"));
+		Modifier modifier = (Modifier)app.getTermService().find(UUID.fromString("efc38dad-205c-4028-ad9d-ae509a14b37a"));
+		CategoricalData cd = CategoricalData.NewInstance();
+		StateData stateData = StateData.NewInstance();
+		stateData.setState(state);
+		stateData.addModifier(modifier);
+		
+		StateData stateData2 = StateData.NewInstance();
+		stateData2.setState(state);
+		stateData2.addModifier(modifier);
+		
+		cd.addState(stateData2);
+		
+		app.getDescriptionService().saveDescriptionElement(cd);
+		System.out.println("Saved");
+		
+		app.commitTransaction(tx);
+		
+		URI	uri = URI.create("file:///C:/localCopy/Data/xper/Cichorieae-DA2.sdd.xml");
+		SDDImportConfigurator configurator = SDDImportConfigurator.NewInstance(uri, destination);
+		CdmDefaultImport<SDDImportConfigurator> myImport = new CdmDefaultImport<SDDImportConfigurator>();
+
+		myImport.setCdmAppController(app);
+		
+		boolean r = myImport.invoke(configurator);
+		System.out.println(r);
+
 		if (true){
 			return;
 		}
@@ -114,24 +112,24 @@ public class TestActivator {
 //		app.changeDataSource(destination);
 //		ICdmDataSource cdmDestination = CdmDestinations.cdm_edit_cichorieae_preview();
 //		app.changeDataSource(cdmDestination);
-		ITaxonServiceConfigurator<?> conf = TaxonServiceConfiguratorImpl.NewInstance();
-		conf.setDoSynonyms(true);
-		conf.setDoTaxa(true);
-		conf.setMatchMode(MatchMode.BEGINNING);
-		conf.setTitleSearchString("L*");
-		conf.setPageNumber(0);
-		conf.setPageSize(50);
-		Set<NamedArea> areas = new HashSet<NamedArea>();
-		areas.add(TdwgArea.getAreaByTdwgAbbreviation("GER"));
-		//conf.setNamedAreas(areas);
-		
-		Pager<IdentifiableEntity> taxaAndSyn = app.getTaxonService().findTaxaAndNames(conf);
-		List<IdentifiableEntity> taxList = taxaAndSyn.getRecords();
-		
-		for (IdentifiableEntity<?> ent: taxList){
-			
-			System.err.println(ent.getTitleCache());
-		}
+//		ITaxonServiceConfigurator<?> conf = TaxonServiceConfiguratorImpl.NewInstance();
+//		conf.setDoSynonyms(true);
+//		conf.setDoTaxa(true);
+//		conf.setMatchMode(MatchMode.BEGINNING);
+//		conf.setTitleSearchString("L*");
+//		conf.setPageNumber(0);
+//		conf.setPageSize(50);
+//		Set<NamedArea> areas = new HashSet<NamedArea>();
+//		areas.add(TdwgArea.getAreaByTdwgAbbreviation("GER"));
+//		//conf.setNamedAreas(areas);
+//		
+//		Pager<IdentifiableEntity> taxaAndSyn = app.getTaxonService().findTaxaAndNames(conf);
+//		List<IdentifiableEntity> taxList = taxaAndSyn.getRecords();
+//		
+//		for (IdentifiableEntity<?> ent: taxList){
+//			
+//			System.err.println(ent.getTitleCache());
+//		}
 		
 	
 		
