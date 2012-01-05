40f24eda5f481a67c9ab59a3028d079717cc84e7
Andreas Müller <a.mueller@bgbm.org>
2012-01-05 23:24:46 +0000

    minor

--- cdm-pesi/src/main/java/eu/etaxonomy/cdm/app/pesi/EuroMedActivator.java
+++ cdm-pesi/src/main/java/eu/etaxonomy/cdm/app/pesi/EuroMedActivator.java
@@ -89,8 +89,8 @@ public class EuroMedActivator {
 	static String occurrenceSourceFilter = " occurrenceFk IN ( SELECT occurrenceId FROM v_cdm_exp_occurrenceAll )"; 
 	static String commonNameFilter = " commonNameId IN ( SELECT commonNameId FROM v_cdm_exp_commonNamesAll )";
 	static String webMarkerFilter = " TableNameFk <> 500 OR ( RIdentifierFk IN (SELECT RIdentifier FROM v_cdm_exp_taxaAll)) ";
-	static String authorTeamFilter = " authorTeamId IN (SELECT authorTeamId FROM v_cdm_exp_authorTeamsAll) ";
-	static String authorFilter = " authorId IN (SELECT authorId FROM v_cdm_exp_authorsAll) "; 
+	static String authorTeamFilter = /*null;*/ " authorTeamId IN (SELECT authorTeamId FROM v_cdm_exp_authorTeamsAll) ";
+	static String authorFilter = /*null;*/" authorId IN (SELECT authorId FROM v_cdm_exp_authorsAll) "; 
 	
 
 	
