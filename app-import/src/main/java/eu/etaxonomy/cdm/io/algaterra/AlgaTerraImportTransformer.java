75614534e02198523e461887878dbc157363230c
Andreas Müller <a.mueller@bgbm.org>
2012-09-13 09:07:46 +0000

    first implementation of Types in AlgaTerra

---
+++ app-import/src/main/java/eu/etaxonomy/cdm/io/algaterra/AlgaTerraImportTransformer.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package eu.etaxonomy.cdm.io.algaterra;
+
+import eu.etaxonomy.cdm.io.berlinModel.BerlinModelTransformer;
+
+/**
+ * @author a.mueller
+ * @created 13.09.2012
+ *
+ */
+public class AlgaTerraImportTransformer extends BerlinModelTransformer {
+	
+}
