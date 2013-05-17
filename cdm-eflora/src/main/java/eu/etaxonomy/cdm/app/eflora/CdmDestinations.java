a0c0c5e02e82f4877821405292635be53781f693
Andreas Müller <a.mueller@bgbm.org>
2013-05-17 12:27:43 +0000

    update db server ip

--- cdm-eflora/src/main/java/eu/etaxonomy/cdm/app/eflora/CdmDestinations.java
+++ cdm-eflora/src/main/java/eu/etaxonomy/cdm/app/eflora/CdmDestinations.java
@@ -268,7 +268,7 @@ public class CdmDestinations {
 	
 	public static ICdmDataSource cdm_flore_gabon_production(){
 		DatabaseTypeEnum dbType = DatabaseTypeEnum.MySQL;
-		String cdmServer = "160.45.63.151";
+		String cdmServer = "160.45.63.171";
 		String cdmDB = "cdm_production_flore_gabon";
 		String cdmUserName = "edit";
 		return makeDestination(dbType, cdmServer, cdmDB, -1, cdmUserName, null);
