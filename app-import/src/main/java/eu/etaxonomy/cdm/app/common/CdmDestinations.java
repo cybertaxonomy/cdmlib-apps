91593c59899f255856e071b1347333d8cce40b08
Katja Luther <k.luther@bgbm.org>
2015-07-04 00:21:53 +0000

    minor

--- app-import/src/main/java/eu/etaxonomy/cdm/app/common/CdmDestinations.java
+++ app-import/src/main/java/eu/etaxonomy/cdm/app/common/CdmDestinations.java
@@ -322,6 +322,14 @@ public class CdmDestinations {
 	public static ICdmDataSource cdm_local_palmae(){
 		DatabaseTypeEnum dbType = DatabaseTypeEnum.MySQL;
 		String cdmServer = "127.0.0.1";
+		String cdmDB = "cdm_edit_caryo";
+		String cdmUserName = "edit";
+		return makeDestination(dbType, cdmServer, cdmDB, -1, cdmUserName, null);
+	}
+	
+	public static ICdmDataSource cdm_local_caryo(){
+		DatabaseTypeEnum dbType = DatabaseTypeEnum.MySQL;
+		String cdmServer = "127.0.0.1";
 		String cdmDB = "cdm_edit_palmae";
 		String cdmUserName = "edit";
 		return makeDestination(dbType, cdmServer, cdmDB, -1, cdmUserName, null);
