2271bdb075f94e49a751b0f2fceac97662a760e7
Andreas Müller <a.mueller@bgbm.org>
2015-08-19 19:35:20 +0200

    Adapt BM datasources to new SQL Server

--- app-import/src/main/java/eu/etaxonomy/cdm/app/berlinModelImport/BerlinModelSources.java
+++ app-import/src/main/java/eu/etaxonomy/cdm/app/berlinModelImport/BerlinModelSources.java
@@ -1,9 +1,9 @@
 
 /**
 * Copyright (C) 2007 EDIT
-* European Distributed Institute of Taxonomy 
+* European Distributed Institute of Taxonomy
 * http://www.e-taxonomy.eu
-* 
+*
 * The contents of this file are subject to the Mozilla Public License Version 1.1
 * See LICENSE.TXT at the top of this package for the full license terms.
 */
@@ -19,8 +19,8 @@ import eu.etaxonomy.cdm.io.common.Source;
 public class BerlinModelSources {
 	@SuppressWarnings("unused")
 	private static final Logger logger = Logger.getLogger(BerlinModelSources.class);
-	
-	
+
+
 
 	public static Source euroMed_PESI3(){
 		//	BerlinModel - Euro+Med
@@ -31,7 +31,7 @@ public class BerlinModelSources {
 		String userName = "pesiexport";
 		return  makeSource(dbms, strServer, strDB, port, userName, null);
 	}
-	
+
 	public static Source iopi(){
 		//	BerlinModel - Euro+Med
 		String dbms = Source.SQL_SERVER_2008;
@@ -41,7 +41,7 @@ public class BerlinModelSources {
 		String userName = "WebUser";
 		return  makeSource(dbms, strServer, strDB, port, userName, null);
 	}
-	
+
 	public static Source euroMed_BGBM42(){
 		//	BerlinModel - Euro+Med
 		String dbms = Source.SQL_SERVER_2008;
@@ -51,7 +51,7 @@ public class BerlinModelSources {
 		String userName = "Webuser";  //was webUser  or pesiexport
 		return  makeSource(dbms, strServer, strDB, port, userName, null);
 	}
-	
+
 	public static Source mcl(){
 		//	BerlinModel - Euro+Med
 		String dbms = Source.SQL_SERVER_2008;
@@ -61,7 +61,7 @@ public class BerlinModelSources {
 		String userName = "WebUser";
 		return  makeSource(dbms, strServer, strDB, port, userName, null);
 	}
-	
+
 	public static Source Campanulaceae(){
 		//	BerlinModel - Campanulaceae
 		String dbms = Source.SQL_SERVER_2005;
@@ -71,7 +71,7 @@ public class BerlinModelSources {
 		String userName = "WebUser";
 		return  makeSource(dbms, strServer, strDB, port, userName, null);
 	}
-	
+
 	public static Source ILDIS(){
 		//	BerlinModel - EditWP6
 		String dbms = Source.SQL_SERVER_2008;
@@ -81,7 +81,7 @@ public class BerlinModelSources {
 		String userName = "webUser";
 		return  makeSource(dbms, strServer, strDB, port, userName, null);
 	}
-	
+
 	public static Source EDIT_CICHORIEAE(){
 		//	BerlinModel - EditWP6
 		String dbms = Source.SQL_SERVER_2005;
@@ -91,7 +91,7 @@ public class BerlinModelSources {
 		String userName = "webUser";
 		return  makeSource(dbms, strServer, strDB, port, userName, null);
 	}
-	
+
 	public static Source EDIT_Taraxacum(){
 		//	BerlinModel - EditWP6
 		String dbms = Source.SQL_SERVER_2005;
@@ -111,38 +111,27 @@ public class BerlinModelSources {
 		String userName = "webUser";
 		return  makeSource(dbms, strServer, strDB, port, userName, null);
 	}
-	
+
 	public static Source El_Salvador(){
 		//	BerlinModel - El_Salvador
-		String dbms = Source.SQL_SERVER_2005;
-		String strServer = "SQL2000Intern";
+		String dbms = Source.SQL_SERVER_2008;
+		String strServer = "BGBM-SQL01";
 		String strDB = "Salvador";
 		int port = 1433;
 		String userName = "WebUser";
 		return  makeSource(dbms, strServer, strDB, port, userName, null);
 	}
-	
-	public static Source El_Salvador_Local(){
-		//	BerlinModel - El_Salvador
-		String dbms = Source.SQL_SERVER_2005;
-		String strServer = "LENOVO-T61";
-		String strDB = "Salvador";
-		int port = 1433;
-		String userName = "salvadorExport";
-		return  makeSource(dbms, strServer, strDB, port, userName, null);
-	}
 
-	
 	public static Source AlgaTerra(){
 		//	BerlinModel - AlgaTerra
 		String dbms = Source.SQL_SERVER_2008;
-		String strServer = "BGBMSQL2008";
+		String strServer = "BGBM-SQL01";
 		String strDB = "Algaterra";
 		int port = 1433;
 		String userName = "WebUser";
 		return  makeSource(dbms, strServer, strDB, port, userName, null);
 	}
-	
+
 	public static Source PESI_ERMS(){
 		//	BerlinModel - Pesi-ERMS
 		String dbms = Source.SQL_SERVER_2005;
@@ -152,7 +141,7 @@ public class BerlinModelSources {
 		String userName = "WebUser";
 		return  makeSource(dbms, strServer, strDB, port, userName, null);
 	}
-	
+
 	public static Source MT_MOOSE(){
 		String dbms = Source.SQL_SERVER_2005;
 		String strServer = "BGBM-SQL02";
@@ -161,7 +150,7 @@ public class BerlinModelSources {
 		String userName = "WebUser";
 		return  makeSource(dbms, strServer, strDB, port, userName, null);
 	}
-	
+
 	public static Source MT_Standardliste(){
 		String dbms = Source.SQL_SERVER_2005;
 		String strServer = "BGBM-SQL02";
@@ -170,7 +159,7 @@ public class BerlinModelSources {
 		String userName = "WebUser";
 		return  makeSource(dbms, strServer, strDB, port, userName, null);
 	}
-	
+
 	/**
 	 * Initializes the source.
 	 * @param dbms
@@ -186,7 +175,7 @@ public class BerlinModelSources {
 		Source source = null;
 		source = new Source(dbms, strServer, strDB);
 		source.setPort(port);
-			
+
 		pwd = AccountStore.readOrStorePassword(dbms, strServer, userName, pwd);
 		source.setUserAndPwd(userName, pwd);
 		// write pwd to account store
--- app-import/src/main/java/eu/etaxonomy/cdm/app/berlinModelImport/SalvadorActivator.java
+++ app-import/src/main/java/eu/etaxonomy/cdm/app/berlinModelImport/SalvadorActivator.java
@@ -1,8 +1,8 @@
 /**
 * Copyright (C) 2007 EDIT
-* European Distributed Institute of Taxonomy 
+* European Distributed Institute of Taxonomy
 * http://www.e-taxonomy.eu
-* 
+*
 * The contents of this file are subject to the Mozilla Public License Version 1.1
 * See LICENSE.TXT at the top of this package for the full license terms.
 */
@@ -14,26 +14,25 @@ import java.util.UUID;
 
 import org.apache.log4j.Logger;
 
-import eu.etaxonomy.cdm.app.berlinModelImport.BerlinModelSources;
 import eu.etaxonomy.cdm.app.common.CdmDestinations;
 import eu.etaxonomy.cdm.database.DbSchemaValidation;
 import eu.etaxonomy.cdm.database.ICdmDataSource;
 import eu.etaxonomy.cdm.io.berlinModel.in.BerlinModelImportConfigurator;
 import eu.etaxonomy.cdm.io.common.CdmDefaultImport;
 import eu.etaxonomy.cdm.io.common.IImportConfigurator;
-import eu.etaxonomy.cdm.io.common.Source;
 import eu.etaxonomy.cdm.io.common.IImportConfigurator.CHECK;
 import eu.etaxonomy.cdm.io.common.IImportConfigurator.DO_REFERENCES;
+import eu.etaxonomy.cdm.io.common.Source;
 import eu.etaxonomy.cdm.model.name.NomenclaturalCode;
 import eu.etaxonomy.cdm.model.name.NonViralName;
 
 
 /**
  * TODO add the following to a wiki page:
- * HINT: If you are about to import into a mysql data base running under windows and if you wish to dump and restore the resulting data bas under another operation systen 
+ * HINT: If you are about to import into a mysql data base running under windows and if you wish to dump and restore the resulting data bas under another operation systen
  * you must set the mysql system variable lower_case_table_names = 0 in order to create data base with table compatible names.
- * 
- * 
+ *
+ *
  * @author a.mueller
  *
  */
@@ -43,30 +42,30 @@ public class SalvadorActivator {
 	//database validation status (create, update, validate ...)
 	static DbSchemaValidation hbm2dll = DbSchemaValidation.CREATE;
 	static final Source berlinModelSource = BerlinModelSources.El_Salvador();
-//	static final ICdmDataSource cdmDestination = CdmDestinations.localH2Salvador();
-	static final ICdmDataSource cdmDestination = CdmDestinations.cdm_test_local_mysql_test();
+	static final ICdmDataSource cdmDestination = CdmDestinations.localH2();
+//	static final ICdmDataSource cdmDestination = CdmDestinations.cdm_test_local_mysql_test();
 	static final UUID treeUuid = UUID.fromString("b010c84d-6049-45f4-9f13-c065101eaa26");
 	static final UUID secUuid = UUID.fromString("d03ef02a-f226-4cb1-bdb4-f6c154f08a34");
 	static final int sourceSecId = 7331;
-	
+
 	static final UUID featureTreeUuid = UUID.fromString("ae9615b8-bc60-4ed0-ad96-897f9226d568");
-	static final Object[] featureKeyList = new Integer[]{302, 303, 306, 307, 309, 310, 311, 312, 350, 1500, 1800, 1900, 1950, 1980, 2000, 10299}; 
-	static boolean isIgnore0AuthorTeam = true;  //special case for Salvador. 
+	static final Object[] featureKeyList = new Integer[]{302, 303, 306, 307, 309, 310, 311, 312, 350, 1500, 1800, 1900, 1950, 1980, 2000, 10299};
+	static boolean isIgnore0AuthorTeam = true;  //special case for Salvador.
 	static boolean doExport = false;
 	static boolean useClassification = true;
-	
+
 	//check - import
 	static final CHECK check = CHECK.CHECK_AND_IMPORT;
 	static final IImportConfigurator.EDITOR editor = IImportConfigurator.EDITOR.EDITOR_AS_EDITOR;
 
 	//NomenclaturalCode
 	static final NomenclaturalCode nomenclaturalCode  = NomenclaturalCode.ICNAFP;
-	
+
 	//ignore null
 	static final boolean ignoreNull = true;
 
 // ****************** ALL *****************************************
-	
+
 	//authors
 	static final boolean doAuthors = true;
 	//references
@@ -77,21 +76,21 @@ public class SalvadorActivator {
 	static final boolean doNameStatus = true;
 	static final boolean doTypes = false;  //Types do not exist in El_Salvador DB
 	static final boolean doNameFacts = false;  //Name Facts do not exist in El_Salvador DB
-	
+
 	//taxa
 	static final boolean doTaxa = true;
 	static final boolean doRelTaxa = true;
 	static final boolean doFacts = true;
 	static final boolean doOccurences = false; //Occurrences do not exist in El_Salvador DB
 	static final boolean doCommonNames = false; //CommonNames do not exist in Salvador DB
-	
+
 	//etc.
 	static final boolean doMarker = true;
 	static final boolean doUser = true;
-		
+
 
 // ************************ NONE **************************************** //
-	
+
 //	//authors
 //	static final boolean doAuthors = false;
 //	//references
@@ -102,32 +101,32 @@ public class SalvadorActivator {
 //	static final boolean doNameStatus = false;
 //	static final boolean doTypes = false;
 //	static final boolean doNameFacts = false;
-//	
+//
 //	//taxa
 //	static final boolean doTaxa = true;
 //	static final boolean doRelTaxa = true;
 //	static final boolean doFacts = false;
 //	static final boolean doOccurences = false;
-//	
+//
 //	//etc.
 //	static final boolean doMarker = false;
-//	static final boolean doUser = false;	
-	
-	
+//	static final boolean doUser = false;
+
+
 	public boolean doImport(ICdmDataSource destination){
 		System.out.println("Start import from BerlinModel("+ berlinModelSource.getDatabase() + ") ...");
-		
+
 		//make BerlinModel Source
 		Source source = berlinModelSource;
-		
+
 		BerlinModelImportConfigurator config = BerlinModelImportConfigurator.NewInstance(source,  destination);
-		
+
 		config.setClassificationUuid(treeUuid);
 		config.setSecUuid(secUuid);
 		config.setSourceSecId(sourceSecId);
 		config.setNomenclaturalCode(nomenclaturalCode);
 		config.setIgnoreNull(ignoreNull);
-		
+
 		config.setDoAuthors(doAuthors);
 		config.setDoReferences(doReferences);
 		config.setDoTaxonNames(doTaxonNames);
@@ -135,34 +134,34 @@ public class SalvadorActivator {
 		config.setDoNameStatus(doNameStatus);
 		config.setDoTypes(doTypes);
 		config.setDoNameFacts(doNameFacts);
-		
+
 		config.setDoTaxa(doTaxa);
 		config.setDoRelTaxa(doRelTaxa);
 		config.setDoFacts(doFacts);
 		config.setDoOccurrence(doOccurences);
 		config.setDoCommonNames(doCommonNames);
-		
+
 		config.setDoMarker(doMarker);
 		config.setDoUser(doUser);
-		
+
 		config.setDbSchemaValidation(hbm2dll);
 
 		config.setCheck(check);
 		config.setEditor(editor);
 		config.setIgnore0AuthorTeam(isIgnore0AuthorTeam);
 		config.setUseClassification(useClassification);
-		
+
 		config.setNamerelationshipTypeMethod(getHandleNameRelationshipTypeMethod());
 		config.setUserTransformationMethod(getTransformUsernameMethod());
-		
+
 		// invoke import
 		CdmDefaultImport<BerlinModelImportConfigurator> bmImport = new CdmDefaultImport<BerlinModelImportConfigurator>();
 		boolean result = bmImport.invoke(config);
 		System.out.println("End import from BerlinModel ("+ source.getDatabase() + ")...");
 		return result;
 	}
-	
-	
+
+
 	/**
 	 * @param args
 	 */
@@ -177,7 +176,7 @@ public class SalvadorActivator {
 		}
 	}
 
-	
+
 	private Method getHandleNameRelationshipTypeMethod(){
 		String methodName = "handleNameRelationshipType";
 		try {
@@ -212,7 +211,7 @@ public class SalvadorActivator {
 			return null;
 		}
 	}
-	
+
 	//used by BerlinModelImportConfigurator
 	@SuppressWarnings("unused")
 	private static String transformUsername(String nameToBeTransformed){
