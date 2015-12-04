f22020bfc6821fa5abb19cb93cfcd2fa4746c2e3
Andreas Müller <a.mueller@bgbm.org>
2015-12-04 16:33:53 +0100

    Revert "Minor"
    This reverts commit 0698ba772c2436c50ee7a27d147d3842fb6aa7e7.

--- cdm-pesi/src/main/java/eu/etaxonomy/cdm/io/pesi/faunaEuropaea/FaunaEuropaeaVernacularName.java
+++ cdm-pesi/src/main/java/eu/etaxonomy/cdm/io/pesi/faunaEuropaea/FaunaEuropaeaVernacularName.java
@@ -4,84 +4,85 @@ import java.util.UUID;
 
 public class FaunaEuropaeaVernacularName {
 
-    String vernacularName;
-    int languageFk;
-    String languageCache;
-    UUID taxonUuid;
-    String guid;
-    String source;
-    String area = "";
+	String vernacularName;
+	int languageFk;
+	String languageCache;
+	UUID taxonUuid;
+	String guid;
+	String source;
+	String area = "";
 
 
-    public FaunaEuropaeaVernacularName(String vernacularName, int languageFK, String languageCache, UUID taxonUuid, String guid, String source){
-        this.guid=guid;
-        this.languageCache = languageCache;
-        this.languageFk = languageFK;
-        this.taxonUuid = taxonUuid;
-        this.vernacularName = vernacularName;
-        this.source = source;
+	public FaunaEuropaeaVernacularName(String vernacularName, int languageFK, String languageCache, UUID taxonUuid, String guid, String source){
+		this.guid=guid;
+		this.languageCache = languageCache;
+		this.languageFk = languageFK;
+		this.taxonUuid = taxonUuid;
+		this.vernacularName = vernacularName;
+		this.source = source;
 
-    }
+	}
 
-    public FaunaEuropaeaVernacularName() {
+	public FaunaEuropaeaVernacularName() {
 
-    }
+	}
 
 
-    public String getArea() {
-        return area;
-    }
+	public String getArea() {
+		return area;
+	}
 
-    public void setArea(String area) {
-        this.area = area;
-    }
+	public void setArea(String area) {
+		this.area = area;
+	}
 
-    public String getVernacularName() {
-        return vernacularName;
-    }
+	public String getVernacularName() {
+		return vernacularName;
+	}
 
-    public void setVernacularName(String vernacularName) {
-        this.vernacularName = vernacularName;
-    }
+	public void setVernacularName(String vernacularName) {
+		this.vernacularName = vernacularName;
+	}
 
-    public int getLanguageFk() {
-        return languageFk;
-    }
+	public int getLanguageFk() {
+		return languageFk;
+	}
 
-    public void setLanguageFk(int languageFk) {
-        this.languageFk = languageFk;
-    }
+	public void setLanguageFk(int languageFk) {
+		this.languageFk = languageFk;
+	}
 
-    public String getLanguageCache() {
-        return languageCache;
-    }
+	public String getLanguageCache() {
+		return languageCache;
+	}
 
-    public void setLanguageCache(String languageCache) {
-        this.languageCache = languageCache;
-    }
+	public void setLanguageCache(String languageCache) {
+		this.languageCache = languageCache;
+	}
 
-    public UUID getTaxonUuid() {
-        return taxonUuid;
-    }
+	public UUID getTaxonUuid() {
+		return taxonUuid;
+	}
 
-    public void setTaxonUuid(UUID taxonUuid) {
-        this.taxonUuid = taxonUuid;
-    }
+	public void setTaxonUuid(UUID taxonUuid) {
+		this.taxonUuid = taxonUuid;
+	}
 
-    public String getGuid() {
-        return guid;
-    }
+	public String getGuid() {
+		return guid;
+	}
 
-    public void setGuid(String guid) {
-        this.guid = guid;
-    }
+	public void setGuid(String guid) {
+		this.guid = guid;
+	}
 
-    public String getSource() {
-        return source;
-    }
+	public String getSource() {
+		return source;
+	}
 
-    public void setSource(String source) {
-        this.source = source;
-    }
+	public void setSource(String source) {
+		this.source = source;
+	}
+
 
 }
