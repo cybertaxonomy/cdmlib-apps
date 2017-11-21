a412044ff8fbf7e473bce61537e57a17a69ad16f
Andreas Müller <a.mueller@bgbm.org>
2017-11-21 10:47:38 +0100

    add test1-3 destinationas

--- app-import/src/main/java/eu/etaxonomy/cdm/app/common/CdmDestinations.java
+++ app-import/src/main/java/eu/etaxonomy/cdm/app/common/CdmDestinations.java
@@ -457,6 +457,28 @@ public class CdmDestinations {
 	}
 
 
+    public static ICdmDataSource cdm_test_test1(){
+        DatabaseTypeEnum dbType = DatabaseTypeEnum.MySQL;
+        String cdmServer = "160.45.63.175";
+        String cdmDB = "cdm_test1";
+        String cdmUserName = "edit";
+        return makeDestination(dbType, cdmServer, cdmDB, -1, cdmUserName, null);
+	}
+    public static ICdmDataSource cdm_test_test2(){
+        DatabaseTypeEnum dbType = DatabaseTypeEnum.MySQL;
+        String cdmServer = "160.45.63.175";
+        String cdmDB = "cdm_test2";
+        String cdmUserName = "edit";
+        return makeDestination(dbType, cdmServer, cdmDB, -1, cdmUserName, null);
+    }
+    public static ICdmDataSource cdm_test_test3(){
+        DatabaseTypeEnum dbType = DatabaseTypeEnum.MySQL;
+        String cdmServer = "160.45.63.175";
+        String cdmDB = "cdm_test3";
+        String cdmUserName = "edit";
+        return makeDestination(dbType, cdmServer, cdmDB, -1, cdmUserName, null);
+    }
+
 	public static ICdmDataSource cdm_test_algaterra(){
 		DatabaseTypeEnum dbType = DatabaseTypeEnum.MySQL;
 		String cdmServer = "160.45.63.175";
