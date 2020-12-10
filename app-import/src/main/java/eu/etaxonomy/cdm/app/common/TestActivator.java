51b088c1bbc52b465f3b2e5a09cd8bdfd5d6b11a
Andreas Müller <a.mueller@bgbm.org>
2020-12-10 22:41:55 +0100

    minor

--- app-import/src/main/java/eu/etaxonomy/cdm/app/common/TestActivator.java
+++ app-import/src/main/java/eu/etaxonomy/cdm/app/common/TestActivator.java
@@ -9,116 +9,109 @@
 
 package eu.etaxonomy.cdm.app.common;
 
-import java.net.URI;
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.EnumSet;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.UUID;
 
 import org.apache.log4j.Logger;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.transaction.TransactionStatus;
 
-import eu.etaxonomy.cdm.api.application.CdmApplicationController;
-import eu.etaxonomy.cdm.common.monitor.DefaultProgressMonitor;
-import eu.etaxonomy.cdm.common.monitor.IProgressMonitor;
+import eu.etaxonomy.cdm.api.service.DistributionTree;
+import eu.etaxonomy.cdm.api.service.dto.DistributionInfoDTO;
+import eu.etaxonomy.cdm.api.service.dto.DistributionInfoDTO.InfoPart;
+import eu.etaxonomy.cdm.api.utility.DistributionOrder;
+import eu.etaxonomy.cdm.common.TreeNode;
 import eu.etaxonomy.cdm.database.DbSchemaValidation;
 import eu.etaxonomy.cdm.database.ICdmDataSource;
-import eu.etaxonomy.cdm.io.common.CdmDefaultImport;
+import eu.etaxonomy.cdm.ext.geo.CondensedDistributionRecipe;
+import eu.etaxonomy.cdm.ext.geo.IEditGeoService;
+import eu.etaxonomy.cdm.io.api.application.CdmIoApplicationController;
 import eu.etaxonomy.cdm.io.common.IImportConfigurator.CHECK;
-import eu.etaxonomy.cdm.io.common.ImportResult;
-import eu.etaxonomy.cdm.io.sdd.in.SDDImportConfigurator;
-import eu.etaxonomy.cdm.model.description.CategoricalData;
-import eu.etaxonomy.cdm.model.description.State;
-import eu.etaxonomy.cdm.model.description.StateData;
-import eu.etaxonomy.cdm.model.name.NomenclaturalCode;
-import eu.etaxonomy.cdm.model.term.DefinedTerm;
+import eu.etaxonomy.cdm.model.common.Language;
+import eu.etaxonomy.cdm.model.common.MarkerType;
+import eu.etaxonomy.cdm.model.description.Distribution;
+import eu.etaxonomy.cdm.model.description.PresenceAbsenceTerm;
+import eu.etaxonomy.cdm.model.location.NamedArea;
+import eu.etaxonomy.cdm.model.location.NamedAreaLevel;
 
 /**
  * @author a.babadshanjan
  * @since 12.05.2009
  */
 public class TestActivator {
+
 	@SuppressWarnings("unused")
 	private static final Logger logger = Logger.getLogger(TestActivator.class);
 
 //	static final ICdmDataSource cdmDestination = CdmDestinations.cdm_test_local_mysql();
 //	static final ICdmDataSource cdmDestination = CdmDestinations.localH2();
-	static final ICdmDataSource cdmDestination = CdmDestinations.cdm_test_test1();
-//	static final ICdmDataSource cdmDestination = CdmDestinations.cdm_edit_cichorieae_preview();
-
-
-
-	static final int limitSave = 2000;
+//	static final ICdmDataSource cdmDestination = CdmDestinations.cdm_test_test1();
+	static final ICdmDataSource cdmDestination = CdmDestinations.cdm_production_euromed();
 
 	static final CHECK check = CHECK.IMPORT_WITHOUT_CHECK;
-	static DbSchemaValidation dbSchemaValidation = DbSchemaValidation.CREATE;
-	static final NomenclaturalCode nomenclaturalCode  = NomenclaturalCode.ICNAFP;
-
-
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-	    new TestActivator().invoke();
-	    System.exit(0);
-	}
+	static DbSchemaValidation dbSchemaValidation = DbSchemaValidation.VALIDATE;
 
 	private void invoke(){
        ICdmDataSource destination = cdmDestination;
 
-        CdmApplicationController app;
+        CdmIoApplicationController app;
 
 //	      applicationEventMulticaster
 //	      app = CdmIoApplicationController.NewInstance(destination, dbSchemaValidation);
 
-        IProgressMonitor progressMonitor = DefaultProgressMonitor.NewInstance();
-        String resourcePath= "/eu/etaxonomy/cdm/appimportTestApplicationContext.xml";
-        ClassPathResource resource = new ClassPathResource(resourcePath);
+//        IProgressMonitor progressMonitor = DefaultProgressMonitor.NewInstance();
+//        String resourcePath= "/eu/etaxonomy/cdm/appimportTestApplicationContext.xml";
+//        ClassPathResource resource = new ClassPathResource(resourcePath);
 //	      ApplicationListener<?> listener = new AppImportApplicationListener();
 //	      List<ApplicationListener> listeners = new ArrayList<>();
 //	      listeners.add(listener);
 //	      app = CdmApplicationController.NewInstance(resource, destination, dbSchemaValidation, false, progressMonitor, listeners);
 //	        app = CdmApplicationController.NewInstance(resource, destination, dbSchemaValidation, false, progressMonitor);
-	        app = CdmApplicationController.NewInstance(destination, dbSchemaValidation, false);
+	        app = CdmIoApplicationController.NewInstance(destination, dbSchemaValidation);
 
-	        if (true){
-	            return;
-	        }
 
-	        TransactionStatus tx = app.startTransaction();
+	        IEditGeoService geoService = (IEditGeoService)app.getBean("editGeoService");
 
-	        State state = (State)app.getTermService().find(UUID.fromString("881b9c80-626d-47a6-b308-a63ee5f4178f"));
-	        DefinedTerm modifier = (DefinedTerm)app.getTermService().find(UUID.fromString("efc38dad-205c-4028-ad9d-ae509a14b37a"));
-	        CategoricalData cd = CategoricalData.NewInstance();
-	        StateData stateData = StateData.NewInstance();
-	        stateData.setState(state);
-	        stateData.addModifier(modifier);
+	        Set<InfoPart> partSet = new HashSet<InfoPart>(Arrays.asList(InfoPart.values()));
+	        EnumSet<InfoPart> parts = EnumSet.copyOf(partSet);
+	        CondensedDistributionRecipe recipe = CondensedDistributionRecipe.EuroPlusMed;
+	        DistributionOrder distributionOrder = DistributionOrder.LABEL;
+	        List<String> propertyPaths = new ArrayList<>();
+	        boolean statusOrderPreference = true;
+	        boolean subAreaPreference = true;
+	        Set<MarkerType> hiddenAreaMarkerTypes = new HashSet<>(); //yy
+	        Map<PresenceAbsenceTerm, Color> presenceAbsenceTermColors = null;
+	        Set<NamedAreaLevel> omitLevels = new HashSet<>();
+	        List<Language> languages = new ArrayList<>();
 
-	        StateData stateData2 = StateData.NewInstance();
-	        stateData2.setState(state);
-	        stateData2.addModifier(modifier);
+	        UUID taxonUuid = UUID.fromString("3e6b5448-4e42-40fa-b13c-a69381b95b13");
+	        DistributionInfoDTO dto = geoService.composeDistributionInfoFor(parts, taxonUuid, subAreaPreference,
+	                statusOrderPreference, hiddenAreaMarkerTypes, omitLevels,
+	                presenceAbsenceTermColors, languages, propertyPaths, recipe, distributionOrder);
 
-	        cd.addStateData(stateData2);
+           DistributionTree tree = dto.getTree();
+           List<TreeNode<Set<Distribution>, NamedArea>> list = tree.toList();
+           System.out.println(list);
 
-	        app.getDescriptionService().saveDescriptionElement(cd);
-	        System.out.println("Saved");
-
-	        app.commitTransaction(tx);
-
-	        URI uri = URI.create("file:///C:/localCopy/Data/xper/Cichorieae-DA2.sdd.xml");
-	        SDDImportConfigurator configurator = SDDImportConfigurator.NewInstance(uri, destination);
-	        CdmDefaultImport<SDDImportConfigurator> myImport = new CdmDefaultImport<>();
-
-	        myImport.setCdmAppController(app);
-
-	        ImportResult result = myImport.invoke(configurator);
-	        System.out.println(result.toString());
-
-	        if (true){
-	            return;
-	        }
-
+//	        URI uri = URI.create("file:///C:/localCopy/Data/xper/Cichorieae-DA2.sdd.xml");
+//	        SDDImportConfigurator configurator = SDDImportConfigurator.NewInstance(uri, destination);
+//	        CdmDefaultImport<SDDImportConfigurator> myImport = new CdmDefaultImport<>();
+//
+//	        myImport.setCdmAppController(app);
+//
+//	        ImportResult result = myImport.invoke(configurator);
+//	        System.out.println(result.toString());
 
 	}
 
 
+    public static void main(String[] args) {
+        new TestActivator().invoke();
+        System.exit(0);
+    }
 }
