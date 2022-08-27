d2973f10b351d2682c76fb0e1f26522440380206
Andreas Müller <a.mueller@bgbm.org>
2022-08-27 13:09:46 +0200

    ref #10092 changes to FloreGabonActivator

--- cdm-eflora/src/main/java/eu/etaxonomy/cdm/app/eflora/FloreGabonActivator.java
+++ cdm-eflora/src/main/java/eu/etaxonomy/cdm/app/eflora/FloreGabonActivator.java
@@ -39,12 +39,12 @@ public class FloreGabonActivator extends EfloraActivatorBase {
 	private static final Logger logger = LogManager.getLogger();
 
 	//database validation status (create, update, validate ...)
-	static DbSchemaValidation hbm2dll = DbSchemaValidation.VALIDATE;
+	static DbSchemaValidation hbm2dll = DbSchemaValidation.CREATE;
 //	static final URI source = EfloraSources.fdg_sample();
 
-	static final ICdmDataSource cdmDestination = CdmDestinations.cdm_flore_gabon_preview();
+//	static final ICdmDataSource cdmDestination = CdmDestinations.cdm_flore_gabon_preview();
 //	static final ICdmDataSource cdmDestination = CdmDestinations.cdm_flore_gabon_production();
-//	static final ICdmDataSource cdmDestination = CdmDestinations.localH2();
+	static final ICdmDataSource cdmDestination = CdmDestinations.localH2();
 //	static final ICdmDataSource cdmDestination = CdmDestinations.cdm_test_local_mysql();
 
 	//feature tree uuid
@@ -71,113 +71,103 @@ public class FloreGabonActivator extends EfloraActivatorBase {
 	//if true, use inverse include information
 	private boolean inverseInclude = true;
 
-	private boolean includeFdg12_17 = true;  //2236  1h17  803t (Rubiaceae)
+	private boolean includeFdg12_17 = false;  //2236  1h17  803t (Rubiaceae1+2)
 	private boolean includeFdg36_37 = true;  //2022  1h8   1627t(824)
-    private boolean includeFdg29 = false;     //1442  //transient (nach 24min)
-    private boolean includeFdg16 = true;     //1355
-    private boolean includeFdg26 = true;     //1290
-    private boolean includeFdg15 = true;  //transient  //1228
-    private boolean includeFdg44 = true;  //1176
-    private boolean includeFdg20 = true;  //1045
+    private boolean includeFdg16 = true ;  //1355   0h52  2078t (451)
+    private boolean includeFdg29 = true;   //1442   2h09  2505t (427)
+    private boolean includeFdg26 = true;   //1290   42m   3044 (539)
+    private boolean includeFdg15 = true;   //1228     1h55  3547 (503)
+    private boolean includeFdg44 = true;   //1176     32m   3974 (427)
+    private boolean includeFdg20 = true;   //1045    51m    4301 (327)
 
-    private boolean includeFdg13 = true;  //transient object
-    private boolean includeFdg5 = true;
-    private boolean includeFdg31 = true;
-    private boolean includeFdg23 = true;
-    private boolean includeFdg24 = true;
-    private boolean includeFdg32 = true;
-    private boolean includeFdg8 = true;
+    private boolean includeFdg13 = true;   //960     33m    4660 (359)
+    private boolean includeFdg5 = true;    //959  27m   5375 (715)
+    private boolean includeFdg5bis = true; //666   //addenda not yet implemented
+    private boolean includeFdg31 = true;   //938   39m     5727 (352)
+    private boolean includeFdg23 = true;   //929   47m    6054 (327)
+    private boolean includeFdg24 = true;   //917  29m   6313 (257)
+    private boolean includeFdg32 = true;   //917  40m  6506 (193)
+    private boolean includeFdg8 = true;    //896 27m  7037(?) (531)
+    private boolean includeFdg19 = true;   //735  23m  7196   (159)
 
-	private boolean includeFdg1 = true;
-	private boolean includeFdg2 = true;
-	private boolean includeFdg3 = true;
-	private boolean includeFdg4 = true;
-	private boolean includeFdg5bis = true;
-	private boolean includeFdg6 = true;
-	private boolean includeFdg7 = true;
-	private boolean includeFdg9 = true;
-	private boolean includeFdg10 = true;
-	private boolean includeFdg11 = true;
+    private boolean includeFdg18 = true;   //676  21m    7366 (170)
+    private boolean includeFdg33 = true;   //611 20m   7532 (166)
+    private boolean includeFdg25 = true;   //592 28m   7694 (162)
+    private boolean includeFdg9 = true;    //572 27m   7992 (298), 3564
+    private boolean includeFdg2 = true;    //486 19m  8157(165) , 3656(92)
+    private boolean includeFdg43 = true;   //483  20m 8282(125), 3739(83)
+    private boolean includeFdg42 = true;   //480  17m 8464(182) , 3824(85)
+    private boolean includeFdg34 = true;   //467  10m 8622(168) , 3877(53)
+    private boolean includeFdg35 = true;   //460  18m 8729(107), 3944(67)
+    private boolean includeFdg45 = true;   //442  17m 8875(146), 4008(64)
+    private boolean includeFdg1 = true;    //425  17m 9067(192), 4105(97)
+    private boolean includeFdg14 = true;   //414  13m 9140(73), 4136(31)
+    private boolean includeFdg30 = true;   //399  16m 9251(111), 4185(49)
+    private boolean includeFdg10 = true;   //394 15m  9461(210), 4326(131)
+    private boolean includeFdg6 = true;    //369  15m 9624(163), 4394(68)
+    private boolean includeFdg11 = true;   //353  18m 9784(160), 4478(84)
+    private boolean includeFdg39 = true;   //352  16m 9873(89), 4545(67)
 
-	private boolean includeFdg14 = true;
+    private boolean includeFdg22 = true;   //330  14m 10058(85), 4593(48)
+    private boolean includeFdg21 = true;   //314 17m 10189(131), 4668(75)
+    private boolean includeFdg41 = true;   //275 14m 10294(105), 4720(52)
+    private boolean includeFdg40 = true;   //273 15m 10386(92), 4773(53)
+    private boolean includeFdg3 = true;    //270 12m 10487(101) 4818(45)
+    private boolean includeFdg7 = true;   //246 15m 10584(97) 4876(58)
+    private boolean includeFdg27 = true;  //244 12m 10707(123), 4916(40)
+    private boolean includeFdg38 = true;   //242 13m 10780(73), 4966(50)
+    private boolean includeFdg28 = true;   //235 10m 10907(127)  5000(34)
+    private boolean includeFdg4 = true;   //227 16m 10973(66), 5036(36)
 
-	private boolean includeFdg18 = true;
-	private boolean includeFdg19 = true;
-	private boolean includeFdg21 = true;
-	private boolean includeFdg22 = true;
-	private boolean includeFdg25 = true;
-
-	private boolean includeFdg27 = true;
-	private boolean includeFdg28 = true;
-
-	private boolean includeFdg30 = true;
-
-	private boolean includeFdg33 = true;
-
-	private boolean includeFdg34 = true;
-	private boolean includeFdg35 = true;
-	private boolean includeFdg38 = true;
-	private boolean includeFdg39 = true;
-	private boolean includeFdg40 = true;
-	private boolean includeFdg41 = true;
-	private boolean includeFdg42 = true;
-	private boolean includeFdg43 = true;
-	private boolean includeFdg45 = true;
-
-	   static final URI fdg1 = EfloraSources.fdg_1();
-	    static final URI fdg2 = EfloraSources.fdg_2();
-	    static final URI fdg3 = EfloraSources.fdg_3();
-	    static final URI fdg4 = EfloraSources.fdg_4();
-	    static final URI fdg5 = EfloraSources.fdg_5();
-	    static final URI fdg5bis = EfloraSources.fdg_5bis();
-	    static final URI fdg6 = EfloraSources.fdg_6();
-	    static final URI fdg7 = EfloraSources.fdg_7();
-	    static final URI fdg8 = EfloraSources.fdg_8();
-	    static final URI fdg9 = EfloraSources.fdg_9();
-	    static final URI fdg10 = EfloraSources.fdg_10();
-	    static final URI fdg11 = EfloraSources.fdg_11();
-	    static final URI fdg12_17 = EfloraSources.fdg_12_17();
-	    static final URI fdg13 = EfloraSources.fdg_13();
-	    static final URI fdg14 = EfloraSources.fdg_14();
-	    static final URI fdg15 = EfloraSources.fdg_15();
-	    static final URI fdg16 = EfloraSources.fdg_16();
-	    static final URI fdg18 = EfloraSources.fdg_18();
-	    static final URI fdg19 = EfloraSources.fdg_19();
-	    static final URI fdg20 = EfloraSources.fdg_20();
-	    static final URI fdg21 = EfloraSources.fdg_21();
-	    static final URI fdg22 = EfloraSources.fdg_22();
-	    static final URI fdg23 = EfloraSources.fdg_23();
-	    static final URI fdg24 = EfloraSources.fdg_24();
-	    static final URI fdg25 = EfloraSources.fdg_25();
-	    static final URI fdg26 = EfloraSources.fdg_26();
-	    static final URI fdg27 = EfloraSources.fdg_27();
-	    static final URI fdg28 = EfloraSources.fdg_28();
-	    static final URI fdg29 = EfloraSources.fdg_29();
-	    static final URI fdg30 = EfloraSources.fdg_30();
-	    static final URI fdg31 = EfloraSources.fdg_31();
-	    static final URI fdg32 = EfloraSources.fdg_32();
-	    static final URI fdg33 = EfloraSources.fdg_33();
-	    static final URI fdg34 = EfloraSources.fdg_34();
-	    static final URI fdg35 = EfloraSources.fdg_35();
-
-	//  static final URI fdg36 = EfloraSources.fdg_36();
-	//  static final URI fdg37 = EfloraSources.fdg_37();
-	    static final URI fdg36_37 = EfloraSources.fdg_36_37();
-
-	    static final URI fdg38 = EfloraSources.fdg_38();
-	    static final URI fdg39 = EfloraSources.fdg_39();
-	    static final URI fdg40 = EfloraSources.fdg_40();
-	    static final URI fdg41 = EfloraSources.fdg_41();
-	    static final URI fdg42 = EfloraSources.fdg_42();
-	    static final URI fdg43 = EfloraSources.fdg_43();
-	    static final URI fdg44 = EfloraSources.fdg_44();
-	    static final URI fdg45 = EfloraSources.fdg_45();
-
+    static final URI fdg1 = EfloraSources.fdg_1();
+    static final URI fdg2 = EfloraSources.fdg_2();
+    static final URI fdg3 = EfloraSources.fdg_3();
+    static final URI fdg4 = EfloraSources.fdg_4();
+    static final URI fdg5 = EfloraSources.fdg_5();
+    static final URI fdg5bis = EfloraSources.fdg_5bis();
+    static final URI fdg6 = EfloraSources.fdg_6();
+    static final URI fdg7 = EfloraSources.fdg_7();
+    static final URI fdg8 = EfloraSources.fdg_8();
+    static final URI fdg9 = EfloraSources.fdg_9();
+    static final URI fdg10 = EfloraSources.fdg_10();
+    static final URI fdg11 = EfloraSources.fdg_11();
+    static final URI fdg12_17 = EfloraSources.fdg_12_17();
+    static final URI fdg13 = EfloraSources.fdg_13();
+    static final URI fdg14 = EfloraSources.fdg_14();
+    static final URI fdg15 = EfloraSources.fdg_15();
+    static final URI fdg16 = EfloraSources.fdg_16();
+    static final URI fdg18 = EfloraSources.fdg_18();
+    static final URI fdg19 = EfloraSources.fdg_19();
+    static final URI fdg20 = EfloraSources.fdg_20();
+    static final URI fdg21 = EfloraSources.fdg_21();
+    static final URI fdg22 = EfloraSources.fdg_22();
+    static final URI fdg23 = EfloraSources.fdg_23();
+    static final URI fdg24 = EfloraSources.fdg_24();
+    static final URI fdg25 = EfloraSources.fdg_25();
+    static final URI fdg26 = EfloraSources.fdg_26();
+    static final URI fdg27 = EfloraSources.fdg_27();
+    static final URI fdg28 = EfloraSources.fdg_28();
+    static final URI fdg29 = EfloraSources.fdg_29();
+    static final URI fdg30 = EfloraSources.fdg_30();
+    static final URI fdg31 = EfloraSources.fdg_31();
+    static final URI fdg32 = EfloraSources.fdg_32();
+    static final URI fdg33 = EfloraSources.fdg_33();
+    static final URI fdg34 = EfloraSources.fdg_34();
+    static final URI fdg35 = EfloraSources.fdg_35();
+    static final URI fdg36_37 = EfloraSources.fdg_36_37();
+    static final URI fdg38 = EfloraSources.fdg_38();
+    static final URI fdg39 = EfloraSources.fdg_39();
+    static final URI fdg40 = EfloraSources.fdg_40();
+    static final URI fdg41 = EfloraSources.fdg_41();
+    static final URI fdg42 = EfloraSources.fdg_42();
+    static final URI fdg43 = EfloraSources.fdg_43();
+    static final URI fdg44 = EfloraSources.fdg_44();
+    static final URI fdg45 = EfloraSources.fdg_45();
 
 // **************** NO CHANGE **********************************************/
 
 	private void doImport(ICdmDataSource cdmDestination){
-		super.doImport(fdg1, cdmDestination,check, h2ForCheck);
+		super.doImport(fdg1, cdmDestination, check, h2ForCheck);
 
 		//make config
 		config.setClassificationUuid(classificationUuid);
@@ -547,6 +537,7 @@ public class FloreGabonActivator extends EfloraActivatorBase {
 	}
 
 	public static void main(String[] args) {
+	    logger.warn("start");
 		FloreGabonActivator me = new FloreGabonActivator();
 		try {
             me.doImport(cdmDestination);
